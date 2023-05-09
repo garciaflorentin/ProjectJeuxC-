41513fadcb343da05af4603f3e68f2764578f56e
florentingarcia <florentin.grc@gmail;com>
2023-05-09 09:46:50 +0200

    commit 09/05(1)

--- Map.cpp
+++ Map.cpp
@@ -2,24 +2,53 @@
 
 Map::Map() {
 
-	map<int, std::vector<Tile*>>* _tiles = new map<int, std::vector<Tile*>>();
-
-	/*// Initialisez _tiles avec des vecteurs vides pour chaque clé.
-	for (int i = 0; i < 10; i++) {
-		(*_tiles)[i] = std::vector<Tile*>();
-	}*/
-	map<int, std::vector<Player*>>* _player = new map<int, std::vector<Player*>>();
-	map<int, std::vector<Chest*>>* _chests = new map<int, std::vector<Chest*>>();
-	map<sf::Texture*, sf::Vector2i>* toDraw = new map<sf::Texture*, sf::Vector2i>();
-	std::vector<Object*>* _map = new std::vector<Object*>();
+toDraw = new std::vector<Object*>;
+_map = new std::vector<Object*>;
 
 }
 
 Map::~Map() {
-	delete _tiles;
-	delete _player;
-	delete _chests;
 	delete toDraw;
 	delete _map;
 
+}
+
+Object* Map::getObject(sf::Vector2f pos){
+
+	
+	for (auto it = _map->begin(); it != _map->end(); ++it) {
+
+		// Vérifier si le pointeur n'est pas nul
+		if (*it) {
+
+			// Accéder à l'objet pointé
+			Object* obj = *it;
+
+			if (obj->getPosition().x == pos.x && obj->getPosition().y == pos.y) {
+				return obj;
+			}
+
+		}
+	}
+
+}
+
+const std::vector<Object*>& Map::objectToDraw(std::vector<sf::Vector2i> currentWindow) {
+
+	for (int _x = currentWindow[1].x; _x < currentWindow[2].x; _x+=48) {
+		for (int _y = currentWindow[1].y; _y < currentWindow[2].y; _y+=48) {
+			toDraw->push_back(getObject(sf::Vector2f(_x, _y)));
+		}
+	}
+
+
+}
+
+void Map::addObject(Object* o) {
+
+	sf::Vector2f pos = o->getPosition();
+
+	if(getObject(pos) != nullptr
+
+	_map->push_back(o);
 }
--- Map.hpp
+++ Map.hpp
@@ -17,35 +17,39 @@ class Map {
 
 private:
 
-	map<int, std::vector<Tile*>>* _tiles;
-	map<int, std::vector<Player*>>* _player;
-	map<int, std::vector<Chest*>>* _chests;
-	// map<int, std::vector<Monster*>> _monsters;
-
-	map<sf::Texture*, sf::Vector2i>* toDraw;
-
+	std::vector<Object*>* toDraw;
 	std::vector<Object*>* _map;
 	
-	enum EnumTile
-	{
-		Grass,
-		Rock,
-		tree
-
-	};
 
 public:
 
 	Map();
 	~Map();
 
-	map<sf::Texture*, sf::Vector2i>* objectToDraw(std::vector<sf::Vector2i> currentWindow) {
+	const std::vector<Object*>& objectToDraw(std::vector<sf::Vector2i> currentWindow);
+
+	void addObject(Object* o);
+	void removeObject(Object* o);
+	
+	void createMap();
+
+	Object* getObject(sf::Vector2f pos);
+
+	std::vector<Object*>* getMap() {
+		return _map;
+	}
 
 
-		for (int i = 0; i < _tiles.size(); i++) {
+	
+};
+#endif
+/* {
+
+		//
+		for (int i = 0; i < _tiles->size(); i++) {
 			for (int a = 0; a < _tiles[i].size(); a++) {
 				//check si la tile est dans la currentWindow ou non
-				if ((_tiles[i][a]->getPosition().x >= currentWindow[1].x || _tiles[i][a]->getPosition().x <= currentWindow[2].x) && (_tiles[i][a]->getPosition().y >= currentWindow[1].y || _tiles[i][a]->getPosition().y <= currentWindow[2].y)) {
+				if ((_tiles.)[i][a]->getPosition().x >= currentWindow[1].x || _tiles[i][a]->getPosition().x <= currentWindow[2].x) && (_tiles[i][a]->getPosition().y >= currentWindow[1].y || _tiles[i][a]->getPosition().y <= currentWindow[2].y)) {
 					// si oui
 					sf::Texture* t = TexturesLib::findTexture(i, 1);
 					toDraw->insert(std::make_pair(t, sf::Vector2i(_tiles[i][a]->getPosition().x, _tiles[i][a]->getPosition().y)));
@@ -69,7 +73,7 @@ public:
 			}
 		}
 	}*/
-
+	/*
 		for (int i = 0; i < _chests.size(); i++) {
 			for (int a = 0; a < _chests[i].size(); a++) {
 				//check si la tile est dans la currentWindow ou non
@@ -88,17 +92,4 @@ public:
 
 		return toDraw;
 	}
-
-	void addObject(int ObjectEnum, int typeEnum, Object* o, sf::Vector2i position);
-	void removeObject(int ObjectEnum, int typeEnum, Object* o, sf::Vector2i position);
-	
-	void createMap();
-
-	std::vector<Object*>* getMap() {
-		return _map;
-	}
-
-
-	
-};
-#endif
+	*/
