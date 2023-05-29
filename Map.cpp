c57817da18c43995ccead2d332d6627ed0664426
Florentin Garcia <florentin.grc@gmail.com>
2023-05-29 19:50:49 +0200

    a

--- Map.cpp
+++ Map.cpp
@@ -428,7 +428,7 @@ void Map::spawnMobs(string area) {
         for (float x = 0; x < 100; x++)
             for (float y = 0; y < 100; y++){
                 if(_monsterList->size()<30){
-           //    if (rand() % SPAWN_FREQUENCY == 0)	addObject(new RangedMonster("MonsterSheet2.png", {x,y}));
+               if (rand() % SPAWN_FREQUENCY == 0)	addObject(new RangedMonster("MonsterSheet2.png", {x,y}));
                if (rand() % SPAWN_FREQUENCY == 0)	addObject(new MeleeMonster("MonsterSheet2.png", {x,y}));
                 }
             }
